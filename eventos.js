b2cb62bb4bd8983b1bb31da825aaf841fb686ffb
Camila Etchebarne <alanthiago468@gmail.com>
2023-08-11 14:56:39 -0300

    Solucion-click

--- eventos.js
+++ eventos.js
@@ -1,3 +1,11 @@
-document.getElementById("bton").addEventListener("click",function()  {
+const div = document.getElementById("divbton")
+const boton = document.getElementById("bton")
+
+div.addEventListener("click",function()  {
     alert("Hola! Soy el div.");
 });
+
+boton.addEventListener("click",function(){
+    alert("Hola!");
+    event.stopPropagation();
+})
